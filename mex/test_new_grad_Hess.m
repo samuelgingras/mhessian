d11f98058ad42db91a22a83e5366556b39885fff
William McCausland <mccauslw@mac.com>
2020-07-07 09:41:33 -0400

    Commit changes before modifying code for new re-centering

---
+++ mex/test_new_grad_Hess.m
@@ -0,0 +1,20 @@
+model = 'gaussian_SV';
+y = load('SP500_return');
+n = length(y);
+u = randn(n,1);
+u = u-mean(u);
+u = n*u/(u'*u);
+
+mu = -10.00;
+phi = 0.9840;
+omega = 40;
+
+theta.N     = n;
+theta.mu    = mu;
+theta.phi   = phi;
+theta.omega = omega;
+
+[hmout, state] = hessianMethod(model, y, theta);
+
+[g, H, V] = grad_hess_approx(theta, state, u);
+[gnew, Hnew, Vnew] = new_grad_hess_approx(theta, state);
