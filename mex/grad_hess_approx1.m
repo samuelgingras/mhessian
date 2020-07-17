97c771281dc9ed42db3f28634f31eea991f6c5fd
William McCausland <mccauslw@mac.com>
2020-07-17 16:10:46 -0400

    New version of grad_hess_approx1.m with full matrix added as 4th argument

--- mex/grad_hess_approx1.m
+++ mex/grad_hess_approx1.m
@@ -1,8 +1,9 @@
 % New approximations of Egrad, EHess and Vgrad
 % New way of setting H(1, 1)
 % New way of (not) setting H(2, 3)
-function [g, H, V] = grad_hess_approx1(theta, state, long_th)
-	[g, H, V] = new_grad_hess_approx(theta, state, long_th);
+function [g, H, V, Hfull] = grad_hess_approx1(theta, state, long_th)
+	[g, Hfull, V] = new_grad_hess_approx(theta, state, long_th);
+	H = Hfull;
 	n = theta.N;
 	H(1, 1) = -n/2;
 	H(1, 2) = 0; H(2, 1) = 0;
--- src/new_grad_hess.h
+++ src/new_grad_hess.h
@@ -3,26 +3,29 @@
 #ifndef MEX_NEW_GRAD_HESS
 #define MEX_NEW_GRAD_HESS
 
+#define p_len 5
+
 typedef struct {
     double Q_11;
     double Q_tt;
     double Q_ttp;
     double q_1;
     double q_t;
-    double m_tm1[5];
-    double m_t[5];
+    double m_tm1[p_len];
+    double m_t[p_len];
     double dQd;
     double qd;
 } Q_term;
 
 typedef struct {
     int i, j;
-    double c_tm1[5];
-    double c_t[5];
+    double c_tm1[p_len];
+    double c_t[p_len];
 } C_term;
 
 void compute_new_grad_Hess(
         const mxArray *mxState,
+        int long_th,
         int n,
         double *mu,
         double phi,
