b011934d3e59ae43792f94fec4b709aaca48ca0b
William McCausland <mccauslw@mac.com>
2023-12-07 11:47:52 -0500

    Eliminate some obsolete crud

--- matlab/MARK_log_prior_eval.m
+++
@@ -1,9 +0,0 @@
-function [lnp_th, lnp_th_g, lnp_th_H] = log_prior_eval(prior, theta)
-
-  if( isfield(theta,'x') )
-    [lnp_th, lnp_th_g, lnp_th_H] = prior.log_prior_eval(prior, theta.x);
-  else
-    [lnp_th, lnp_th_g, lnp_th_H] = prior.log_prior_eval(prior, theta);
-  end
-  
-end
--- matlab/MARK_simulate_sample.m
+++
@@ -1,4 +0,0 @@
-function y = simulate_sample(model,theta)
-    x = drawState( theta );
-    y = drawObs( x, model, theta );
-end
